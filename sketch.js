cc41a5c552942922915e9c0b892faa1f0097f05e
kaua1110 <gaida.kaua@escola.pr.gov.br>
2024-08-26 09:54:58 -0300

    adicionando arquivo ls e css

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup () {
+    createCanvas(600,600)
+    
+    
+ 
+ background ("black")
+    
+  }
+ 
+ function draw(){
+   
+   
+   
+   stroke("white")
+   
+   fill ("rgb(9,208,236)")
+   
+   
+   
+ //console.log(mouseIsPressed)
+   
+   
+   
+   if (mouseIsPressed) {
+     
+     rect(mouseX, mouseY, 20, 35)
+   }
+ }
+   
+   
+ 
